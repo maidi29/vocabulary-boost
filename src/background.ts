7567837dea67abe1def842ec22ada11db43c3ce4
Mareike <mareike.haug@web.de>
2022-02-28 20:29:24 +0100

    selection and translation functionality

--- src/background.ts
+++ src/background.ts
@@ -4,4 +4,34 @@ chrome.action.onClicked.addListener(function(tab) {
     });
 });
 
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+    fetchTranslation(request.word).then(sendResponse);
+    return true;
+});
+
+const fetchTranslation = async (word: string) => {
+    let url = new URL('https://api-free.deepl.com/v2/translate');
+    const params = {
+        auth_key: '96dfdf8c-64a8-66e3-799c-d66a731ebfef:fx',
+        text: word,
+        target_lang: 'DE',
+        source_lang: 'EN'
+    }
+    url.search = new URLSearchParams(params).toString();
+    const fetchUrl = url.toString();
+
+    const translation = await fetch(fetchUrl, {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .catch(error => console.log("error", error));
+
+    console.log('translation :', JSON.stringify(translation));
+    return translation;
+}
+
 export {};
--- src/contentscript.ts
+++ src/contentscript.ts
@@ -1,7 +1,43 @@
 document.addEventListener('mouseup', ()=> {
-        const selection = window.getSelection()?.toString();
-        console.log('selection: ', selection);
+        const selection = window.getSelection();
+        const word = selection?.toString().split(' ')[0]
+        const position =  selection?.getRangeAt(0)?.getBoundingClientRect();
+        if (word && word.length < 50 && position) {
+            requestTranslation(word, position);
+        } else {
+            console.log('Error with selection');
+        }
     }
 );
+
+const requestTranslation = (word: string, position: DOMRect) => {
+    chrome.runtime.sendMessage({word: word}, (response) => {
+        const translations = response?.translations.map((translation: any) => translation.text).join(", ");
+        showTooltip(translations, response.translations[0].detected_source_language, position);
+    })
+}
+
+const showTooltip = (message: string, detectedSourceLang: string, position: DOMRect) => {
+    const id = 'vocabulary-boost-overlay';
+    document.getElementById(id)?.remove();
+
+    const tooltip = document.createElement('div');
+    tooltip.setAttribute('id', id);
+    tooltip.setAttribute('style', `
+        position: absolute; 
+        top: ${position.top + window.scrollY}px; 
+        left: ${position.left}px; 
+        background: white; 
+        transform: translate(0, -100%); 
+        padding: 1rem;
+        font-size: 1rem;
+        color: #333;`)
+    tooltip.innerHTML = `
+        <div>${message}</div>
+        <div>Übersetzt aus: ${detectedSourceLang}</div>
+    `;
+
+    document.body.appendChild(tooltip);
+}
 
 export {};
