270f44f071d899e9de85cae061d0bf1959d907ae
Haug, Mareike <mareike.haug@interhyp.de>
2022-03-09 21:12:41 +0100

    some logic and design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import './App.scss';
 import {IndexCard} from "./components/IndexCard/IndexCard";
 import {IndexCardSide, Variants} from "./components/IndexCard/IndexCardSide/IndexCardSide";
-import {PractiseContext, PractiseContextProvider, PractiseStates, withPractiseContext} from "./PractiseContext";
+import {PractiseContext, PractiseStates, withPractiseContext} from "./PractiseContext";
 
 export const AppContainer = () => {
     const { state, setState, word, setWord } = useContext(PractiseContext);
+    const [ trainingSet, setTrainingSet ] = useState([
+        {
+            sentence: "A mouse is in the house.",
+            sentenceTranslation: "Im Haus ist eine Maus.",
+            translation: "Maus",
+            word: "Mouse",
+            occurance: "https://skjdfnskjdfn"
+        },{
+            sentence: "A dog is in the house.",
+            sentenceTranslation: "Im Haus ist ein Hund.",
+            translation: "Hund",
+            word: "Dog",
+            occurance: "https://skjdfnskjdfn"
+        }
+    ]);
+    const [ index, setIndex ] = useState(0);
+    const [ counter, setCounter ] = useState(0);
+    const [input, setInput] = useState('');
+
+
+    chrome.storage?.sync?.get(['trainingSet'], (result) => {
+        setTrainingSet(result.trainingSet);
+        setWord(result.trainingSet[index]);
+        console.log(result);
+    });
+
+    setWord(trainingSet[index]);
+
+
 
     /*Todo:
     - Nutzer im options tab Sprache einstellen lassen
@@ -21,33 +50,76 @@ export const AppContainer = () => {
      */
 
     const switchToNextWord = () => {
-        chrome.storage.sync.get(['trainingSet'], (result) => {console.log(result)});
-        setWord({
-            sentence: "A mouse is in the house.",
-            sentenceTranslation: "Im Haus ist eine Maus.",
-            translation: "Maus",
-            word: "Mouse",
-            occurance: "https://skjdfnskjdfn"
-        })
+        console.log('switch', index);
+        const newIndex = index + 1;
+        if (newIndex < trainingSet.length) {
+            setIndex(newIndex);
+            setInput('');
+            setState(PractiseStates.INITIAL);
+        } else {
+            console.log('no more words');
+        }
+
     }
+
+    useEffect(() => {
+        if (trainingSet) {
+            setWord(trainingSet[index]);
+        }
+    }, [index]);
+
+    useEffect(() => {
+        console.log('state: ' , state);
+    }, [state]);
+
+    const renderInput = () =>
+        <><input placeholder="Translation" value={input} onInput={e => setInput((e.target as HTMLInputElement).value)}/>
+            <button onClick={()=>{
+                setState(PractiseStates.TO_VERIFY);
+                setState(input.trim().toLowerCase() === word.translation.toLowerCase() ? PractiseStates.CORRECT : PractiseStates.WRONG);
+            }}>Verify!</button></>;
+
+    const renderCard = () => {
+        switch (state) {
+            case PractiseStates.INITIAL:
+                return renderInput();
+            case PractiseStates.WRONG:
+                return <>
+                    <div className={'wrong'}>{input}</div>
+                    <div>The word stays in the training set and will be presented later again.</div>
+                </>;
+            case PractiseStates.CORRECT:
+                return <>
+                    <div className={'correct'}>{input}</div>
+                    <div>🥳 Yey! The word is added to the archive.</div>
+                </>;
+            case PractiseStates.TO_VERIFY:
+                return renderInput();
+        }
+    }
+
   return (
-      <div>
-        <IndexCard flipped={state !== PractiseStates.INITIAL}>
-                <IndexCardSide variant={Variants.FRONT}>
-                    <div>{word.word}</div>
-                    <div>{word.sentence}</div>
-                </IndexCardSide>
-                <IndexCardSide variant={Variants.BACK}>
-                    <div>{word.translation}</div>
-                    <div>{word.sentenceTranslation}</div>
-                </IndexCardSide>
-        </IndexCard>
-        <IndexCard>
-            <IndexCardSide variant={Variants.FRONT}>
-                <input placeholder="Translation"/>
-                <button onClick={()=>setState(PractiseStates.TO_VERIFY)}>Verify!</button>
-            </IndexCardSide>
-        </IndexCard>
+      <div className="app-container">
+          <h1>Practise the words you learned while browsing</h1>
+          <h2>Total number of learned words: {counter}</h2>
+          <div className="card-container">
+                <IndexCard flipped={state !== PractiseStates.INITIAL}>
+                    <IndexCardSide variant={Variants.FRONT}>
+                        <div>{word.word}</div>
+                        <div>{word.sentence}</div>
+                        <div>Learned at <a href={word.occurance}>{word.occurance}</a></div>
+                    </IndexCardSide>
+                    <IndexCardSide variant={Variants.BACK}>
+                        <div>{word.translation}</div>
+                        <div>{word.sentenceTranslation}</div>
+                    </IndexCardSide>
+                </IndexCard>
+                <IndexCard>
+                    <IndexCardSide variant={Variants.FRONT}>
+                        {renderCard()}
+                    </IndexCardSide>
+                </IndexCard>
+          </div>
           <button onClick={switchToNextWord}>Next word</button>
     </div>
 
